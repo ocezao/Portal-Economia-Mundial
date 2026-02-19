d8ee08ad260da446c95fdfe911aee7beaf36d083
ocezao <ocezao@gmail.com>
2026-02-19 17:26:58 -0300

    feat: adicionar Google Analytics, Clarity e Google Tag Manager
    - Google Analytics 4 (gtag.js) - ID: G-842VLRHHXQ
    - Microsoft Clarity - ID: vjzgusrh04
    - Google Tag Manager - ID: GTM-PBXFKLFR
    - GTM noscript iframe apos tag body

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -109,8 +109,52 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
           src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-6096980902806551"
           crossOrigin="anonymous"
         />
+        
+        {/* Google tag (gtag.js) */}
+        <script async src="https://www.googletagmanager.com/gtag/js?id=G-842VLRHHXQ"></script>
+        <script dangerouslySetInnerHTML={{
+          __html: `
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', 'G-842VLRHHXQ');
+          `
+        }} />
+        
+        {/* Microsoft Clarity */}
+        <script dangerouslySetInnerHTML={{
+          __html: `
+            (function(c,l,a,r,i,t,y){
+                c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
+                t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
+                y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
+            })(window, document, "clarity", "script", "vjzgusrh04");
+          `
+        }} />
+        
+        {/* Google Tag Manager */}
+        <script dangerouslySetInnerHTML={{
+          __html: `
+            (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+            new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+            j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+            'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+            })(window,document,'script','dataLayer','GTM-PBXFKLFR');
+          `
+        }} />
       </head>
       <body className="min-h-screen bg-white text-[#111111] antialiased">
+        {/* Google Tag Manager (noscript) */}
+        <noscript>
+          <iframe 
+            src="https://www.googletagmanager.com/ns.html?id=GTM-PBXFKLFR"
+            height="0" 
+            width="0" 
+            style={{ display: 'none', visibility: 'hidden' }}
+          />
+        </noscript>
+        {/* End Google Tag Manager (noscript) */}
+        
         <Providers>
           {children}
         </Providers>
